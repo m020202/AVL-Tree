6b804b54a6f62fcbb9bf176971bacf349debf71e
m020202 <m020202@naver.com>
2024-11-17 12:38:30 +0900

    Feat: AVLTree 인터페이스 정의

---
+++ AVLTree.h
@@ -0,0 +1,12 @@
+class AVLTree {
+public:
+    virtual void Find(int x) = 0;
+    virtual void Insert(int x) = 0;
+    virtual void Empty() = 0;
+    virtual void Size() = 0;
+    virtual void Height() = 0;
+    virtual void Ancestor() = 0;
+    virtual void Average() = 0;
+    virtual void Rank() = 0;
+    virtual void Erase(int x) = 0;
+};
