daef79f18ed6dc6355d24a69911fedfb2944f987
m020202 <m020202@naver.com>
2024-11-17 13:06:03 +0900

    Fix: #pragma 적용

--- AVLTree.h
+++ AVLTree.h
@@ -1,3 +1,5 @@
+#pragma once
+
 class AVLTree {
 public:
     virtual void Find(int x) = 0;
@@ -5,8 +7,9 @@ public:
     virtual void Empty() = 0;
     virtual void Size() = 0;
     virtual void Height() = 0;
-    virtual void Ancestor() = 0;
-    virtual void Average() = 0;
-    virtual void Rank() = 0;
+    virtual void Ancestor(int x) = 0;
+    virtual void Average(int x) = 0;
+    virtual void Rank(int x) = 0;
     virtual void Erase(int x) = 0;
+    virtual void PrintTree() = 0;
 };
