48600374702ccea4218f207f8084b9436861a029
m020202 <m020202@naver.com>
2024-11-17 13:06:28 +0900

    Feat: Adaptee를 AVLTree에 연결시킬 Adapter 클래스 구현

---
+++ AVLTreeAdapter.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <cmath>
+#include <queue>
+#include <iomanip>
+#include "AVLTree.h"
+#include "AVLTreeAdaptee.h"
+using namespace std;
+
+class AVLTreeAdapter : public AVLTree{
+public:
+    AVLTreeAdapter(AVLTreeAdaptee adaptee) :
+        adaptee_(adaptee) {}
+
+    void Find(int x) override {
+        cout << adaptee_.Finding(x) << endl;
+    }
+    void Insert(int x) override {
+        cout << adaptee_.Inserting(x) << endl;
+    }
+    void Empty() override {
+        cout << adaptee_.IsEmpty() << endl;
+    }
+    void Size() override {
+        cout << adaptee_.GetSize() << endl;
+    }
+    void Height() override {
+        cout << adaptee_.GetHeight() << endl;
+    }
+    void Ancestor(int x) override {
+        adaptee_.GetAncestor(x);
+    }
+    void Average(int x) override {
+        adaptee_.GetAverage(x);
+    }
+    void Rank(int x) override {
+        adaptee_.GetRank(x);
+    }
+    void Erase(int x) override {
+        adaptee_.Erasing(x);
+    }
+
+    void PrintTree() override {
+        adaptee_.PrintTree();
+    }
+
+private:
+    AVLTreeAdaptee adaptee_;
+};
+
