66f7553bf6bc791664e691cae10374ccc6ab0038
m020202 <m020202@naver.com>
2024-11-17 13:07:07 +0900

    Feat: 예시 테스트 코드 구현

--- main.cpp
+++ main.cpp
@@ -1,54 +1,67 @@
 #include <iostream>
+#include "AVLTreeAdaptee.h"
 #include "AVLTree.h"
+#include "AVLTreeAdapter.h"
 using namespace std;
 
 int main() {
-    AVLTree tree;
+    AVLTreeAdaptee adaptee;
+    AVLTree* tree = new AVLTreeAdapter(adaptee);
 
     // Insert test cases
-    tree.Inserting(10);
-    tree.Inserting(20);
-    tree.Inserting(30);
-    tree.Inserting(15);
-    tree.Inserting(5);
+    cout << "Insert(10): ";
+    tree->Insert(10);
+    cout << "Insert(20): ";
+    tree->Insert(20);
+    cout << "Insert(30): ";
+    tree->Insert(30);
+    cout << "Insert(15): ";
+    tree->Insert(15);
+    cout << "Insert(5): ";
+    tree->Insert(5);
 
     // 현재 트리 출력
     cout << "=========현재 트리=========" << endl;
-    tree.PrintTree();
+    tree->PrintTree();
     cout << "=========================" << endl;
 
     // Size
-    cout << "Size of tree: " << tree.GetSize() << endl;
+    cout << "Size of tree: ";
+    tree->Size();
 
     // Find depth + height of a node
-    cout << "Find 20: " << tree.Finding(20) << endl;
+    cout << "Find 20: ";
+    tree->Find(20);
 
     // Ancestor
     cout << "Ancestor(15): ";
-    tree.GetAncestor(15);
+    tree->Ancestor(15);
 
     // Average test case (min + max value in subtree)
     cout << "Average(10): ";
-    tree.GetAverage(10);
+    tree->Average(10);
 
     // Tree height
-    cout << "Tree Height: " << tree.GetHeight() << endl;
+    cout << "Tree Height: ";
+    tree->Height();
 
     // Check if tree is empty
-    cout << "Is tree empty? " << tree.IsEmpty() << endl;
+    cout << "Is tree empty? ";
+    tree->Empty();
 
     // Rank
     cout << "Rank(30): ";
-    tree.GetRank(30);
+    tree->Rank(30);
 
     // Remove a node
     cout << "Erase 20: ";
-    tree.Erasing(20);
+    tree->Erase(20);
 
-    cout << "Size after removing 20: " << tree.GetSize() << endl;
+    cout << "Size after removing 20: ";
+    tree->Size();
 
     cout << "=========현재 트리=========" << endl;
-    tree.PrintTree();
+    tree->PrintTree();
     cout << "=========================" << endl;
 
     return 0;
